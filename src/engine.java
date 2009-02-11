655e144163e0ba17cb8f50cf8825be81c6b56bac
Amit Prakash Ambasta <amit.prakash.ambasta@gmail.com>
2009-02-11 23:00:22 +0530

    New Engine Model

---
+++ src/engine.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.lang.*;
+
+public class engine
+{
+  public static void main(String[] args)
+  {
+  	System.out.println("Git Merger");
+  	System.out.println("Hello World");
+  }
+}
